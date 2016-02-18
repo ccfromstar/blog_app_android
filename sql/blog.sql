4676e35f8e79d6f6bb6577107c3db24f683d451d
Uncle CHEN <114413511@qq.com>
2016-02-18 21:15:26 +0800

    sql

---
+++ sql/blog.sql
@@ -0,0 +1,31 @@
+﻿DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `page` varchar(50) DEFAULT NULL,
+  `time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `ip` varchar(50) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13470 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `post` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `category_id` int(11) NOT NULL DEFAULT '0',
+  `state_id` int(11) NOT NULL DEFAULT '0',
+  `needcode` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=166 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `posts_category`;
+CREATE TABLE `posts_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8;
+
+DROP VIEW IF EXISTS `posts_all`;
+CREATE VIEW `posts_all` AS 
+  select `a`.`id` AS `id`,`a`.`title` AS `title`,`a`.`post` AS `post`,`a`.`created_at` AS `created_at`,`a`.`category_id` AS `category_id`,`a`.`state_id` AS `state_id`,`a`.`needcode` AS `needcode`,`b`.`name` AS `name` from (`posts` `a` left join `posts_category` `b` on((`a`.`category_id` = `b`.`id`)));
